7d449a81922f325a1968360d746a7e495f622980
Mark Craven <mpcrav01@gmail.com>
2024-10-20 08:38:15 -0400

    basic db schema setup

---
+++ server/supabase/seed.sql
@@ -0,0 +1,38 @@
+insert into leagues (name, sport)
+values ('NFL', 'American Football');
+
+
+
+insert into teams (name, league, intraleague_details, abbrv, logo_url)
+values ('Cincinatti Bengals', 1, 'AFC North', 'CIN', 'bengals.png'),
+('Baltimore Ravens', 1, 'AFC North', 'BAL', 'ravens.png'),
+('Pittsburgh Steelers', 1, 'AFC North', 'PIT', 'steelers.png'),
+('Cleveland Browns', 1, 'AFC North', 'CLE', 'browns.png'),
+('Buffalo Bills', 1, 'AFC East', 'BUF', 'bills.png'),
+('Miami Dolphins', 1, 'AFC East', 'MIA', 'dolphins.png'),
+('New England Patriots', 1, 'AFC East', 'NE', 'patriots.png'),
+('New York Jets', 1, 'AFC East', 'NYJ', 'jets.png'),
+('Houston Texans', 1, 'AFC South', 'HOU', 'texans.png'),
+('Indianapolis Colts', 1, 'AFC South', 'IND', 'colts.png'),
+('Jacksonville Jaguars', 1, 'AFC South', 'JAX', 'jaguars.png'),
+('Tennessee Titans', 1, 'AFC South', 'TEN', 'titans.png'),
+('Denver Broncos', 1, 'AFC West', 'DEN', 'broncos.png'),
+('Kansas City Chiefs', 1, 'AFC West', 'KC', 'chiefs.png'),
+('Las Vegas Raiders', 1, 'AFC West', 'LV', 'raiders.png'),
+('Los Angeles Chargers', 1, 'AFC West', 'LAC', 'chargers.png'),
+('Dallas Cowboys', 1, 'NFC East', 'DAL', 'cowboys.png'),
+('New York Giants', 1, 'NFC East', 'NYG', 'giants.png'),
+('Philadelphia Eagles', 1, 'NFC East', 'PHI', 'eagles.png'),
+('Washington Commanders', 1, 'NFC East', 'WAS', 'commanders.png'),
+('Chicago Bears', 1, 'NFC North', 'CHI', 'bears.png'),
+('Detroit Lions', 1, 'NFC North', 'DET', 'lions.png'),
+('Green Bay Packers', 1, 'NFC North', 'GB', 'packers.png'),
+('Minnesota Vikings', 1, 'NFC North', 'MIN', 'vikings.png'),
+('Atlanta Falcons', 1, 'NFC South', 'ATL', 'falcons.png'),
+('Carolina Panthers', 1, 'NFC South', 'CAR', 'panthers.png'),
+('New Orleans Saints', 1, 'NFC South', 'NO', 'saints.png'),
+('Tampa Bay Buccaneers', 1, 'NFC South', 'TB', 'buccs.png'),
+('Arizona Cardinals', 1, 'NFC West', 'ARI', 'cardinals.png'),
+('Los Angeles Rams', 1, 'NFC West', 'LAR', 'rams.png'),
+('San Francisco 49ers', 1, 'NFC West', 'SF', 'niners.png'),
+('Seattle Seahawks', 1, 'NFC West', 'SEA', 'seahawks.png');
